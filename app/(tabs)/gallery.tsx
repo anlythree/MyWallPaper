936f5aded7ea4b61f17fbdbfdd2bc90f91d68f40
anlythree <15610462126@163.com>
2024-03-13 18:20:10 +0800

    创建好需要的3个tab

--- app/(tabs)/gallery.tsx
+++ app/(tabs)/gallery.tsx
@@ -6,9 +6,9 @@ import { Text, View } from '@/components/Themed';
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Me</Text>
+      <Text style={styles.title}>Gallery</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/me.tsx" />
+      <EditScreenInfo path="app/(tabs)/gallery.tsx" />
     </View>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,13 +3,15 @@ import {Button, StyleSheet} from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import {Link} from "expo-router";
-
+/*
+*注意! 这个index.tsx的名字是不能改的，这是整个系统的入口文件，react靠这个名字来识别进入app后首先展示的页面!
+* */
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Today</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/today.tsx" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
       {/*  一种点击按钮打印日志的方法 */}
       <Button onPress={()=>console.log('------>')} title={'testPress'}></Button>
       <Link href={'/wallpaper'}>press to Wall Paper!</Link>
