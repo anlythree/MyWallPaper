567075d13b2968f4d409e5db34d0f928ca000884
anlythree <15610462126@163.com>
2024-03-14 15:31:59 +0800

    删掉没用的Text

--- app/(tabs)/gallery.tsx
+++ app/(tabs)/gallery.tsx
@@ -22,8 +22,6 @@ export default function TabTwoScreen() {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Gallery</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
             {/*<EditScreenInfo path="app/(tabs)/gallery.tsx" />*/}
             <Text>动态获取墙纸数据，并用FlatList显示</Text>
             <Text>通过过滤器，选择只显示收藏的壁纸，需要考虑本地储存壁纸的问题</Text>
