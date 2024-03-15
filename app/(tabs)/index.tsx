f997943d9d22854bdb24f09066997bdfca2f522c
anlythree <15610462126@163.com>
2024-03-15 15:00:48 +0800

    code sync

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,18 +4,21 @@ import {Text, View} from '@/components/Themed';
 import {getBingWallPapers} from "@/utils/api";
 import React, {useEffect, useState} from "react";
 import {BlurView} from "expo-blur";
-import {Link, Tabs} from "expo-router";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import Colors from "@/constants/Colors";
+import {Tabs} from "expo-router";
 /*
 *注意! 这个index.tsx的名字是不能改的，这是整个系统的入口文件，react靠这个名字来识别进入app后首先展示的页面!
 * */
 export default function TabOneScreen() {
 
+    // 请求的所有图片
+    const [allWallPaperImage,setAllWallPaperImage] = useState<any>([])
+    // 当前图片在所有图片中的下标
+    const [showWallPaperIndex, setShowWallPaperIndex] = useState<number>(0)
     // 壁纸图片，默认undefind
     const [wallPaperImage, setWallPaperImage] = useState<string | undefined>(undefined)
     // 是不是加载状态
     const [isLoading, setIsLoading] = useState<boolean>(false)
+
 
     // 通过useEffect的方式配置初次进入页面时就加载当天的图片
     useEffect(() => {
@@ -27,8 +30,10 @@ export default function TabOneScreen() {
     const handleGetBingImage = async () => {
         setIsLoading(true)
         const arr = await getBingWallPapers({n: 1})
-        const url = `https://bing.com${arr[0].url}`
-        console.log(url)
+        setAllWallPaperImage(arr)
+        console.log('获取所有壁纸对象列表--->'+allWallPaperImage)
+        const url = `https://bing.com${allWallPaperImage[0].url}`
+        console.log('当前壁纸图片url--->'+url)
         setWallPaperImage(url)
         setIsLoading(false)
     }
@@ -44,7 +49,7 @@ export default function TabOneScreen() {
                     headerTransparent:true
                 }}
             />
-            <View style={styles.container}>
+            <View style={[styles.container,{width:'100%'}]}>
                 {
                     // 在loading显示loading图标，不在loading图片url为空不显示，不为空显示
                     isLoading ?
